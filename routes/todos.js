975d868ca5c1c6957e19fc1f7b837942778a44eb
programmer-alt <modstils88@mail.ru>
2024-02-08 18:47:49 +0400

    create .hbs added todos.js router.get for create

--- routes/todos.js
+++ routes/todos.js
@@ -1,7 +1,10 @@
 const { Router } = require('express')
 const router = Router()
 router.get('/', (req, res) => {
-    res.render('index')
+    res.render('index' )
+})
+router.get('/create', (req, res)=> {
+    res.render('create')
 })
 
 module.exports = router
