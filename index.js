25b32e35f1afa0031ea5cd0f2965049563486099
programmer-alt <modstils88@mail.ru>
2024-03-11 16:52:05 +0400

    comments for files

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // Импортируем модуль Express и сохраняем его в переменную express
 const express = require('express');
-const exphbs = require('express-handlebars')
+// Импортируем модуль Express Handlebars и сохраняем его в переменную exphbs
+const exphbs = require('express-handlebars');
 
 // Импортируем модуль Mongoose и сохраняем его в переменную mongoose
 const mongoose = require('mongoose');
-const TodoRoutes = require ('./routes/todos')
+// Импортируем маршруты задач (todos) и сохраняем их в переменную TodoRoutes
+const TodoRoutes = require('./routes/todos');
 
 // Определяем переменную PORT, которая будет использоваться для прослушивания порта сервера.
 // Если в окружении установлена переменная среды PORT, используем ее значение, иначе используем  3000.
@@ -12,26 +14,31 @@ const PORT = process.env.PORT ||  3000;
 
 // Создаем новый экземпляр приложения Express и сохраняем его в переменную app
 const app = express();
-const hbs = exphbs.create ({
-    defaultLayout: 'main',
-    extname: 'hbs'
-})
-app.engine('hbs', hbs.engine)
-app.set('view engine', 'hbs')
-app.set('views', 'views')
+// Создаем новый экземпляр движка шаблонов Express Handlebars с настройками по умолчанию
+const hbs = exphbs.create({
+    defaultLayout: 'main', // Устанавливаем основной макет для шаблонов
+    extname: 'hbs' // Расширение файлов шаблонов
+});
+// Регистрируем движок шаблонов Express Handlebars в приложении Express
+app.engine('hbs', hbs.engine);
+// Устанавливаем движок шаблонов Express Handlebars в качестве основного движка шаблонов для приложения Express
+app.set('view engine', 'hbs');
+// Указываем директорию, где Express будет искать файлы шаблонов
+app.set('views', 'views');
 
-app.use(TodoRoutes)
+// Используем маршруты задач (todos) в приложении Express
+app.use(TodoRoutes);
 
 // Функция start асинхронно выполняет подключение к базе данных MongoDB с помощью Mongoose
-async function start () {
+async function start() {
     try {
-        // Подключаемся к MongoDB с пустым URL, что некорректно и должно быть исправлено
+        // Подключаемся к MongoDB с помощью строки подключения
         await mongoose.connect('mongodb+srv://tom:vadim1988@cluster0.tw0doh9.mongodb.net/todos', {
             // Включаем новый парсер URL для обработки строк подключения
             useNewUrlParser: true,
+            // Отключаем использование устаревшей опции useUnifiedTopology
             useUnifiedTopology: false
             // Отключаем использование метода findAndModify, который может вызвать проблемы совместимости
-            
         });
 
         // Запускаем сервер на порту, определенном в переменной PORT
--- routes/todos.js
+++ routes/todos.js
@@ -1,10 +1,37 @@
-const { Router } = require('express')
-const router = Router()
-router.get('/', (req, res) => {
-    res.render('index' )
-})
-router.get('/create', (req, res)=> {
-    res.render('create')
-})
+// Импортируем Router из библиотеки express для создания маршрутов
+const { Router } = require('express');
+// Импортируем модель Todo из файла Todo.js
+const Todo = require('../models/Todo');
+// Создаем экземпляр роутера
+const router = Router();
 
-module.exports = router
+// Определяем маршрут GET для корневого URL ('/')
+router.get('/', async (req, res) => {
+    // Используем асинхронную функцию для обработки запроса
+    // Получаем все документы из коллекции Todo в базе данных
+    const todos = await Todo.find({});
+    
+    // Отправляем ответ с рендером страницы 'index' и передаем данные в шаблон
+    res.render('index', {
+        // Заголовок страницы
+        title: 'Todo List',
+        // Флаг, указывающий, что это главная страница
+        isIndex: true,
+        // Передаем полученные задачи в шаблон
+        todos
+    });
+});
+
+// Определяем маршрут GET для URL '/create'
+router.get('/create', (req, res) => {
+    // Отправляем ответ с рендером страницы 'create' и передаем данные в шаблон
+    res.render('create', {
+        // Заголовок страницы
+        title: 'Create todo',
+        // Флаг, указывающий, что это страница создания новой задачи
+        isCreate: true
+    });
+});
+
+// Экспортируем созданный роутер для использования в других частях приложения
+module.exports = router;
